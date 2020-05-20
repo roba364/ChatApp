1ff64e8186c10aef1fb49037042e340fb2a0732e
Sergey <borovkovsergey364@gmail.com>
2020-05-20 12:48:09 +0300

    - setup root navController - start to setup navigationController UI

---
+++ ChatApp/ChatApp/Controllers/ConversationsController.swift
@@ -0,0 +1,56 @@
+//
+//  ConversationsController.swift
+//  ChatApp
+//
+//  Created by SofiaBuslavskaya on 20/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        
+    }
+    
+    //MARK: - Selectors
+    
+    @objc func showProfile() {
+        print("123")
+    }
+    
+    //MARK: - Helpers
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        configureNavigationBar()
+        
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    private func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+    }
+}
