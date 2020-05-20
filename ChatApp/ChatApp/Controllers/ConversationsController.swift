18ed33ce0622beeaebd3ad4b74ef2372cecb7da7
Sergey <borovkovsergey364@gmail.com>
2020-05-20 13:18:56 +0300

    - configured tableView programmatically

--- ChatApp/ChatApp/Controllers/ConversationsController.swift
+++ ChatApp/ChatApp/Controllers/ConversationsController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     
     //MARK: - Properties
+    
+    private let tableView = UITableView()
     
     //MARK: - Lifecycle
     
@@ -31,11 +35,29 @@ class ConversationsController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
+        
         configureNavigationBar()
+        configureTableView()
         
         
         let image = UIImage(systemName: "person.circle.fill")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(showProfile))
+    }
+    
+    private func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        
+        tableView.frame = view.frame
     }
     
     private func configureNavigationBar() {
@@ -52,5 +74,32 @@ class ConversationsController: UIViewController {
         navigationItem.title = "Messages"
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
+}
+
+extension ConversationsController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "test cell"
+        
+        return cell
+    }
+    
 }
