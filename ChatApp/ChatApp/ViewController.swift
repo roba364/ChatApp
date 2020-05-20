7d475327971dd43fa2ae8da8d7e9136339447e5a
Sergey <borovkovsergey364@gmail.com>
2020-05-20 12:09:45 +0300

    - start new project

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by SofiaBuslavskaya on 20/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
