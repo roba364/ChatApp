87d78f39840a3c9f24ef132976c2b3ca0bc22877
Sergey <borovkovsergey364@gmail.com>
2020-05-21 18:25:26 +0300

    - created Message Model - configured MessageCell using bubbleContainer - realized MessageViewModel preparing data for MessageCell

---
+++ ChatApp/ChatApp/Model/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by SofiaBuslavskaya on 21/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import Foundation
---
+++ ChatApp/ChatApp/View/MessageCell.swift
@@ -0,0 +1,9 @@
+//
+//  MessageCell.swift
+//  ChatApp
+//
+//  Created by SofiaBuslavskaya on 21/05/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import Foundation
